68cf04976c4606695183297d10e8b0b4da51fe46
Nick Addison <nick@addisonbrown.com.au>
2015-02-28 13:47:47 +1100

    week 1 assignment

---
+++ week1/day2/profile/profile/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  profile
+//
+//  Created by Nicholas Addison on 26/02/2015.
+//  Copyright (c) 2015 Nicholas Addison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func unwindFromSegue (segue: UIStoryboardSegue) {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
